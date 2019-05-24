fe1c971de6067581fc85ba712c02452fbcf04e5c
remorses <beats.by.morse@gmail.com>
2019-05-24 13:09:37 +0200

    supports multilines around types

--- skema/tokenize.py
+++ skema/tokenize.py
@@ -1,18 +1,29 @@
 
 
 from .parser import Tokenizer, EOF_TOKEN
+from functools import reduce
 import json
 
 Tokenizer.__next__ = Tokenizer.get_next_token
 Tokenizer.__iter__ = lambda self: iter(self.get_next_token, EOF_TOKEN)
 
 
+def remove_duplicates(acc, token):
+  if acc and acc[-1]['type'] == 'SEPARATOR' and token['type'] == 'SEPARATOR':
+    return acc
+  else:
+    return acc + [token]
+   
+
+
+
 def tokenize(string):
   tokenizer = Tokenizer(string)
   tokens = [t for t in tokenizer]
-  while tokens[0]['type'] == 'SEPARATOR':
+  tokens = reduce(remove_duplicates, tokens, [])
+  if tokens[0]['type'] == 'SEPARATOR':
     tokens = tokens[1:]
-  while tokens[-1]['type'] == 'SEPARATOR':
+  if tokens[-1]['type'] == 'SEPARATOR':
     tokens = tokens[:-1]
   return tokens
 
