a04eaeb18923c982baee48d802107d67b51375df
remorses <beats.by.morse@gmail.com>
2019-07-24 19:42:05 +0200

    fix bug with just

--- skema/fake_data/hypo_schema.py
+++ skema/fake_data/hypo_schema.py
@@ -143,7 +143,7 @@ def get_generator(prop, customs={}):
         title = prop.get('title', '').strip()
         if title in customs:
             # print(customs[title]())
-            return hs.just(customs[title]())
+            return hs.builds(customs[title])
         else:
             prop = {k:v for k,v in prop.items() if k != 'title'}
     
