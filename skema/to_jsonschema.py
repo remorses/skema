4781acbf086674f0a68879840e7615d98187bf69
remorses <beats.by.morse@gmail.com>
2019-09-15 14:48:15 +0200

    fix

--- skema/to_jsonschema.py
+++ skema/to_jsonschema.py
@@ -8,10 +8,6 @@ from .support import rcompose
 def to_jsonschema(schema, ref=None, resolve=False, hide=[], only=None):
 
     node = make_tree(tokenize(schema))
-    # if only and isinstance(only, list):
-    #     node.children = [c for c in node.children if c.value in only]
-    # if hide and isinstance(hide, list):
-    #     node.children = [c for c in node.children if not c.value in hide]
     result = make_schema(node)
     if ref and not ref in result.get("definitions", {}):
         raise Exception(f"can't find definition {ref}")
@@ -25,10 +21,7 @@ def to_jsonschema(schema, ref=None, resolve=False, hide=[], only=None):
             k: v for k, v in result["definitions"].items() if not k in hide
         }
 
-    reference = result["$ref"] if not ref else "#/definitions/" + ref
-    if reference not in result["definitions"].keys():
-        reference = list(result["definitions"].keys())[0]
-    result["$ref"] = reference
+    result["$ref"] = result["$ref"] if not ref else "#/definitions/" + ref
 
     if resolve:
         resolve_refs(result)
