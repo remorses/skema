1ebf8feb33af69fdecaa3337ef448a3c1dc5ddc5
remorses <beats.by.morse@gmail.com>
2019-09-16 11:50:58 +0200

    skema/fake_data/__init__:

--- skema/fake_data/__init__.py
+++ skema/fake_data/__init__.py
@@ -11,8 +11,11 @@ from .hypo_schema import get_generator
 
 
 
-def fake_data(schema: str, ref=None, amount=5, cutom_types={}) -> List[dict]:
-    json_schema=to_jsonschema(schema, ref=ref, resolve=True)
+def fake_data(schema: str, ref=None, amount=5, cutom_types={}, from_json=False) -> List[dict]:
+    if from_json:
+        json_schema=schema
+    else:
+        json_schema=to_jsonschema(schema, ref=ref, resolve=True)
     examples = []
     @given(get_generator(json_schema, cutom_types))
     @settings(max_examples=amount)
