32ad00afca6841d51ec57888cbb95b5b3a5c4d79
remorses <beats.by.morse@gmail.com>
2019-11-07 20:29:41 +0100

    skema/reconstruct/graphql:

--- skema/reconstruct/graphql.py
+++ skema/reconstruct/graphql.py
@@ -1,21 +1,12 @@
-try:
-    from graphql import build_schema, DocumentNode
-    from graphql.language.ast import (
-        Node,
-        ObjectTypeDefinitionNode,
-        UnionTypeDefinitionNode,
-        ScalarTypeDefinitionNode,
-        FragmentDefinitionNode,
-        EnumTypeDefinitionNode,
-        FieldDefinitionNode,
-    )
-except ImportError:
-    raise Exception('need graphql-core package to reconstruct graphql')
-from .print_graphql_schema import print_schema
 from skema.parser import parse
 from lark import Tree
 
 def reconstruct(string) -> Tree:
+    try:
+        from graphql import build_schema, DocumentNode
+        from .print_graphql_schema import print_schema
+    except ImportError:
+        raise Exception('need graphql-core package to reconstruct graphql')
     schema = build_schema(string)
     skema = print_schema(schema)
     print(skema.replace(' ', '.'))
