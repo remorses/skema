ab8a63bf65d4f8132711fdb65de35e81b5634c42
remorses <beats.by.morse@gmail.com>
2019-11-09 22:22:12 +0100

    tests/test_skema: added test

--- tests/test_skema.py
+++ tests/test_skema.py
@@ -5,4 +5,11 @@ from .support import *
 def test_1(x):
     code = gens.skema(parse(x))
     print(code)
+
+@pytest.mark.parametrize("x", schemas, ids=names)
+def test_can_parse(x):
+    code = gens.skema(parse(x))
+    print(code)
+    t = parse(code)
+    print(t.pretty())
     
